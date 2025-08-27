e9b35938da383cd03f535b4b5c0ce7b2a3c2e8fa
ctsao1 <ctsao1@hwemail.com>
2025-08-27 13:31:54 -0700

    End of Class 8/27

--- PugRescue/GoldenSaver.java
+++ PugRescue/GoldenSaver.java
@@ -5,10 +5,8 @@ public class GoldenSaver {
 
 	//Moves every dog whose breed is "Golden ____" in the list to the back of the list
 	public static void rescueGolden(ArrayList<Dog> list) {
-		ArrayList<Dog> sorted = new ArrayList<Dog>();
 		for (int i = 0; i < list.size(); i++) {
-			boolean movedDog = false;
-			while (movedDog == false) {
+			if (list.get(i).getBreed().toLowerCase().indexOf("gold") != -1) {
 				
 			}
 		}
