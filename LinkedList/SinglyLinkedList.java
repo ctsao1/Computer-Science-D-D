871767a03838d07dc66823129664fddfdbccb1f2
ctsao1 <ctsao1@hwemail.com>
2025-09-19 11:45:19 -0700

    End of Class 9/19

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -8,11 +8,19 @@ public class SinglyLinkedList<E> {
 
 	// Constructor: creates an empty list
 	public SinglyLinkedList() {
+		E [] internalArray = (E[]) new Object[0];
 	}
 
 	// Constructor: creates a list that contains
 	// all elements from the array values, in the same order
 	public SinglyLinkedList(Object[] values) {
+		int j = 0;
+		head.setValue((E) values[0]);
+		tail.setValue((E) values[values.length - 1]);
+		E [] internalArray = (E[]) new Object[values.length];
+		for (ListNode i = new L; i.equals(tail) != false; i = i.getNext()) {
+			
+		}
 	}
 	
 	public ListNode<E> getHead() {
@@ -25,25 +33,33 @@ public class SinglyLinkedList<E> {
 
 	// Returns true if this list is empty; otherwise returns false.
 	public boolean isEmpty() {
+		if (nodeCount == 0) {
+			return true;
+		}
+		return false;
 	}
 
 	// Returns the number of elements in this list.
 	public int size() {
+		return nodeCount;
 	}
 
 	// Returns true if this list contains an element equal to obj;
 	// otherwise returns false.
 	public boolean contains(E obj) {
+
 	}
 
 	// Returns the index of the first element in equal to obj;
 	// if not found, returns -1.
 	public int indexOf(E obj) {
+
 	}
 
 	// Adds obj to this collection.  Returns true if successful;
 	// otherwise returns false.
 	public boolean add(E obj) {
+
 	}
 
 	// Removes the first element that is equal to obj, if any.
@@ -54,15 +70,18 @@ public class SinglyLinkedList<E> {
 
 	// Returns the i-th element.               
 	public E get(int i) {
+
 	}
 
 	// Replaces the i-th element with obj and returns the old value.
 	public E set(int i, Object obj) {
+
 	}
 
 	// Inserts obj to become the i-th element. Increments the size
 	// of the list by one.
 	public void add(int i, Object obj) {
+
 	}
 
 	// Removes the i-th element and returns its value.
