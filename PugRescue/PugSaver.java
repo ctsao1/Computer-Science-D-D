4acdbc2272a0c09464cbbde6287f6671e3903e14
ctsao1 <ctsao1@hwemail.com>
2025-10-07 11:00:15 -0700

    Finished PugSaver + MyArrayList

--- PugRescue/PugSaver.java
+++ PugRescue/PugSaver.java
@@ -6,17 +6,17 @@ public class PugSaver {
 	//Moves every dog whose breed is "Pug" in the list to the back of the list
 	//All non-pugs must remain in the same relative order they were in originally
 	//and all pugs must also remain in the same relative order they were in originally
-	public static void rescuePugs(ArrayList<Dog> list) {
+	public static void rescuePugs(MyArrayList<Dog> list) {
 		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).getName().toLowerCase().indexOf("golden") == -1) {
-				int index = 0;
-				for (int j = list.size(); j > 0; j--) {
-					if (list.get(i).getName().toLowerCase().indexOf("golden") != -1) {
-						index = i;
-						i = -1;
+			if (list.get(i).getName().toLowerCase().indexOf("golden") == 1) {
+				int index = i;
+				int backIndex = 0;
+				for (int j = list.size() - 1; j > 0; j--) {
+					if (list.get(j).getName().toLowerCase().indexOf("golden") != -1) {
+						backIndex = j;
 					}
 				}
-				list.set(i, list.set(index, list.get(i)));
+				list.set(backIndex, list.set(index, list.get(backIndex)));
 			}
 		}
 	}
--- PugRescue/Tester.java
+++ PugRescue/Tester.java
@@ -1,16 +1,31 @@
 public class Tester {
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        MyArrayList list = new MyArrayList<>();
-        list.add("list");
-        System.out.println(list.toString());
-        list.add("bozo");
-        System.out.println(list.toString());
-        list.add(1, "BOZO");
-        System.out.println(list.toString());
-        System.out.println(list.remove(0));
-        System.out.println(list.toString());
-        System.out.println(list.remove("BOZO"));
-        System.out.println(list.toString());
+        // MyArrayList list = new MyArrayList<>();
+        // list.add("list");
+        // System.out.println(list.toString());
+        // list.add("bozo");
+        // System.out.println(list.toString());
+        // list.add(1, "BOZO");
+        // System.out.println(list.toString());
+        // System.out.println(list.remove(0));
+        // System.out.println(list.toString());
+        // System.out.println(list.remove("BOZO"));
+        // System.out.println(list.toString());
+
+        Dog golden = new Dog("Joey", "Golden Retriever");
+        Dog doge = new Dog("Doge", "Doge");
+        MyArrayList<Dog> dogLine = new MyArrayList<>(100000000);
+        for (int i = 0; i < 100000000; i++) {
+            if (i % 2 == 1) {
+                dogLine.add(golden);
+            } else {
+                dogLine.add(doge);
+            }
+        }
+        PugSaver.rescuePugs(dogLine);
+
+        System.out.println(dogLine.get(99999999));
+        System.out.println(dogLine.get(50000001));
     }
 }
