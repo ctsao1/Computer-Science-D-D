7ec91345b90181ca577c7f12f8f7d342a72ed98f
ctsao1 <ctsao1@hwemail.com>
2026-01-21 14:28:15 -0800

    End of Class 1/21

--- MyStack/Arithmetic.java
+++ MyStack/Arithmetic.java
@@ -3,13 +3,28 @@ public class Arithmetic {
 
 	//Evaluates a String exp that has an arithmetic expression, written in classic notation
 	public static int evaluate(String exp) {
-		return 0;
+		String[] str = exp.split(" ");
+		return operate(Integer.parseInt(str[0]), Integer.parseInt(str[2]), str[1]);
 	}
 	
 	//Returns the result of doing operand1 operation operand2,
 	//e.g. operate(5, 2, "-") should return 3
 	public static int operate(int operand1, int operand2, String operation) {
-		return 0;
+		if (operation.equals("+")) {
+			return operand1 + operand2;
+		}
+		if (operation.equals("-")) {
+			return operand1 - operand2;
+		}
+		if (operation.equals("*")) {
+			return operand1 * operand2;
+		}
+		if (operation.equals("^")) {
+			return operand1^operand2;
+		}
+		else {
+			return operand1 / operand2;
+		}
 	}
 	
 	//Evaluates a String exp that has an arithmetic expression written in STOUT notation
