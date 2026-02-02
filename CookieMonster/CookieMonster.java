7f0a92a6db505fae27a3e29c901573941aa387c9
ctsao1 <ctsao1@hwemail.com>
2026-02-02 01:25:40 -0800

    Genius

--- CookieMonster/CookieMonster.java
+++ CookieMonster/CookieMonster.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.*;
 import java.lang.Math;
+import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Stack;
 
 // You are allowed (and expected!) to use either Java's ArrayDeque or LinkedList class to make stacks and queues
 
@@ -76,8 +79,24 @@ public class CookieMonster {
 	 * Returns the maximum number of cookies attainable. */
     /* From any given position, always add the path right before adding the path down */
     public int queueCookies() {
-		//CODE THIS
-		return 0;
+		int cookies = 0;
+		Queue<OrphanScout> orphans = new ArrayDeque<OrphanScout>();
+		orphans.offer(new OrphanScout(0, 0, cookieGrid[0][0]));
+		while (!orphans.isEmpty()) {
+			int row = orphans.peek().getEndingRow();
+			int col = orphans.peek().getEndingCol();
+			if (validPoint(row, col + 1)) {
+				orphans.offer(new OrphanScout(row, col + 1, orphans.peek().getCookiesDiscovered() + cookieGrid[row][col + 1]));
+			}
+			if (validPoint(row + 1, col)) {
+				orphans.offer(new OrphanScout(row + 1, col, orphans.peek().getCookiesDiscovered() + cookieGrid[row + 1][col]));
+			}
+			if (!validPoint(row + 1, col + 1)) {
+				cookies = Math.max(cookies, orphans.peek().getCookiesDiscovered());
+			}
+			orphans.poll();
+		}
+		return cookies;
     }
 
     
@@ -85,8 +104,25 @@ public class CookieMonster {
  	 * Returns the maximum number of cookies attainable. */
     /* From any given position, always add the path right before adding the path down */
     public int stackCookies() {
-		//CODE THIS
-		return 0;
+		int cookies = 0;
+		Stack<OrphanScout> orphans = new Stack<OrphanScout>();
+		orphans.push(new OrphanScout(0, 0, cookieGrid[0][0]));
+		while (!orphans.empty()) {
+			OrphanScout clone = orphans.pop();
+			int row = clone.getEndingRow();
+			int col = clone.getEndingCol();
+			int cookiesDiscovered = clone.getCookiesDiscovered();
+			if (validPoint(row, col + 1)) {
+				orphans.push(new OrphanScout(row, col + 1, cookiesDiscovered + cookieGrid[row][col + 1]));
+			}
+			if (validPoint(row + 1, col)) {
+				orphans.push(new OrphanScout(row + 1, col, cookiesDiscovered + cookieGrid[row + 1][col]));
+			}
+			if (!validPoint(row + 1, col + 1)) {
+				cookies = Math.max(cookies, cookiesDiscovered);
+			}
+		}
+		return cookies;
     }
 
 }
