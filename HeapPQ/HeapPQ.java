68f2491b806a5fc01cca304c310de8ddc312106e
ctsao1 <ctsao1@hwemail.com>
2026-02-10 09:16:33 -0800

    End of Class 2/10

--- HeapPQ/HeapPQ.java
+++ HeapPQ/HeapPQ.java
@@ -11,9 +11,13 @@ public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
 	}
 
 	//Returns the number of elements in the priority queue
-	public int size()
-	{
-		
+	public int size() {
+		for (int i = 0; i < heap.length; i++) {
+			if (heap[i] == null) {
+				return i;
+			}
+		}
+		return heap.length;
 	}
 
 	//DO NOT CHANGE MY JANKY TOSTRING!!!!!
@@ -51,38 +55,59 @@ public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
 	}
 
 	//Doubles the size of the heap array
-	private void increaseCapacity()
-	{
-
+	private void increaseCapacity() {
+		E[] temp = (E[])new Comparable[objectCount * 2];
+		for (int i = 0; i < objectCount; i++) {
+			temp[i] = heap[i];
+		}
+		objectCount = objectCount * 2;
 	}
 
 	//Returns the index of the "parent" of index i
-	private int parent(int i)
-	{
-
+	private int parent(int i) {
+		return (i - 1) / 2;
 	}
-	//Returns the index of the *smaller child* of index i
-	private int smallerChild(int i)
-	{
 
+	//Returns the index of the *smaller child* of index i
+	private int smallerChild(int i) {
+		return 0;
 	}
 
 	//Swaps the contents of indices i and j
-	private void swap(int i, int j)
-	{
-
+	private void swap(int i, int j) {
+		E temp = heap[i];
+		heap[i] = heap[j];
+		heap[j] = temp;
 	}
 
 	// Bubbles the element at index i upwards until the heap properties hold again.
-	private void bubbleUp(int i)
-	{
+	private void bubbleUp(int i) {
 
 	}
 
 	// Bubbles the element at index i downwards until the heap properties hold again.
-	private void bubbleDown(int i)
-	{
+	private void bubbleDown(int i) {
 
+	}
+
+	@Override
+	public void add(E obj) {
+		throw new UnsupportedOperationException("Unimplemented method 'add'");
+	}
+
+	@Override
+	public E removeMin() {
+		throw new UnsupportedOperationException("Unimplemented method 'removeMin'");
+	}
+
+	@Override
+	public E peek() {
+		return heap[0];
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size() == 0;
 	}
 
 }
---
+++ HeapPQ/PriorityQueue/MyPQ.java
@@ -0,0 +1,27 @@
+public class MyPQ<E extends Comparable<E>> implements MyPriorityQueue<E>{
+
+    @Override
+    public void add(E obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public E removeMin() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeMin'");
+    }
+
+    @Override
+    public E peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+	
+}
