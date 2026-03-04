22180f609e3573d81c5ea5887ee67f4e6ea35a78
ctsao1 <ctsao1@hwemail.com>
2026-03-04 13:31:34 -0800

    End of Class 4/3

---
+++ Markov-2/MarkovPrediction.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class MarkovPrediction {
+    
+    public MarkovPrediction() {
+
+    }
+
+    // Please define your own variables and data structures
+    // 
+    public ArrayList<String[]> readData(String filePath) {
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // Method to predict the next state given a current state
+    public String predictNextState(String currentState) {
+
+
+    }
+
+}
