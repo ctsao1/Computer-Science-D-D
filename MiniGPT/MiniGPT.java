ec6cc73487936c9ff85def002e397cb2e6f71742
ctsao1 <ctsao1@hwemail.com>
2026-03-11 09:17:42 -0700

    I'm GPTing it

--- MiniGPT/MiniGPT.java
+++ MiniGPT/MiniGPT.java
@@ -4,11 +4,39 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MiniGPT {
 
-	public MiniGPT(String fileName, int chainOrder) {
+	private HashMap<String, ArrayList<Character>> map; 
 
+	public MiniGPT(String fileName, int chainOrder) {
+		map = new HashMap<>();
+		try (BufferedReader reader = new BufferedReader(new FileReader("sample.txt"))) {
+            int charAsInt;
+            // Read until the end of the stream (-1 is returned)
+			StringBuilder str = new StringBuilder();
+            while ((charAsInt = reader.read()) != -1) {
+                // Cast the integer value to a character
+                Character character = (char) charAsInt;
+				if (str.toString().length() < chainOrder) {
+					str.append(character);
+				} else {
+					if (map.containsKey(str.toString())) {
+						map.get(str.toString()).add(character);
+					} else {
+						ArrayList<Character> list = new ArrayList<>();
+						list.add(character);
+						map.put(str.toString(), list);
+					}
+				}
+				str.deleteCharAt(0);
+            }
+        } catch (IOException e) {
+            System.err.println("An I/O error occurred: " + e.getMessage());
+        }
+        return map;
 	}
 
 	
